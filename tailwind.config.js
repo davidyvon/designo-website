f76493e870d06bb9358a861088c5dc2a74174eb0
David Yvon <david.yvon@pelostud.io>
2023-01-17 13:14:31 +0100

    Add colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,14 @@
 module.exports = {
 	content: ['./app/**/*.{js,jsx,ts,tsx}', './pages/**/*.{js,jsx,ts,tsx}'],
 	theme: {
-		extend: {},
+		colors: {
+			peach: '#E7816B',
+			peach_light: '#FFAD9B',
+			gray: '#F1F3F5',
+			gray_dark: '#333136',
+			black: '#1D1C1E',
+			white: '#FFFFFF',
+		},
 	},
 	plugins: [],
 }
