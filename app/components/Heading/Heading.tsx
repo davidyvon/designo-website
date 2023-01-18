22d61a233cfa17ed5bfbf20a37176162fe5f1a52
David Yvon <david.yvon@pelostud.io>
2023-01-18 14:40:28 +0100

    Add <Heading/>

--- app/components/Heading/Heading.tsx
+++ app/components/Heading/Heading.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { storyblokEditable } from '@storyblok/react'
+import { render } from 'storyblok-rich-text-react-renderer-ts'
 
 type HeadingProps = {
 	blok: {
@@ -7,21 +8,30 @@ type HeadingProps = {
 		_uid: string
 		_editable?: string
 
-		title?: string
-		tag?: 1 | 2 | 3 | 4 | 5 | 6
+		heading?: string
+		description?: string
 	}
 }
 
 const Heading = ({ blok }: HeadingProps): JSX.Element => {
-	const { title, tag } = blok
-
-	const Tag: string = tag ? `h${tag}` : 'p'
-	const Title = Tag as 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6' | 'p'
+	const { heading, description } = blok
 
 	return (
-		<div className='grid-container' {...storyblokEditable(blok)}>
-			<div>{title && <Title>{title}</Title>}</div>
-		</div>
+		<section
+			className='grid-container mb-16 md:mb-20 bg-peach md:bg-white'
+			{...storyblokEditable(blok)}
+		>
+			<div className='grid-container py-28 mx:py-16 md:px-0 bg-peach text-white rounded-2xl col-start-1 col-end-5 md:col-end-9 lg:col-end-13'>
+				<div className='flex flex-col justify-center items-center gap-6 text-center col-start-1 col-end-5 md:col-start-3 md:col-end-7 lg:col-start-4 lg:col-end-10'>
+					{heading && (
+						<div className='[&>*]:text-heading-m md:[&>*]:text-heading-xl'>
+							{render(heading)}
+						</div>
+					)}
+					{description && <p>{description}</p>}
+				</div>
+			</div>
+		</section>
 	)
 }
 
