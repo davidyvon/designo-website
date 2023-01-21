b359262e87951ec2b164d16a3c0ca63693039c27
David Yvon <david.yvon@pelostud.io>
2023-01-21 13:14:53 +0100

    Add Google Fonts link

--- pages/[...slug].tsx
+++ pages/[...slug].tsx
@@ -28,6 +28,16 @@ export default function Page({ story }: PageProps) {
 			<Head>
 				<title>{story ? `Next Starter | ${story.name}` : 'Next Starter'}</title>
 				<meta name='description' content='Next Starter Project' />
+				<link rel='preconnect' href='https://fonts.googleapis.com' />
+				<link
+					rel='preconnect'
+					href='https://fonts.gstatic.com'
+					crossOrigin=''
+				/>
+				<link
+					href='https://fonts.googleapis.com/css2?family=Jost:wght@400;500&display=swap'
+					rel='stylesheet'
+				/>
 			</Head>
 
 			<StoryblokComponent blok={story.content} />
--- pages/index.tsx
+++ pages/index.tsx
@@ -30,6 +30,16 @@ const Home = ({ story }: HomeProps) => {
 					{story ? `Next Starter | ${story.name}` : 'Next Starter | Home'}
 				</title>
 				<meta name='description' content='Next Starter Projet' />
+				<link rel='preconnect' href='https://fonts.googleapis.com' />
+				<link
+					rel='preconnect'
+					href='https://fonts.gstatic.com'
+					crossOrigin=''
+				/>
+				<link
+					href='https://fonts.googleapis.com/css2?family=Jost:wght@400;500&display=swap'
+					rel='stylesheet'
+				/>
 			</Head>
 
 			<StoryblokComponent blok={story.content} />
